51c3828d9ce178665d448b0d2c5a438ba59a982d
Ye15 <yeshao0105@gmail.com>
2016-02-25 00:36:26 +0800

    Http

---
+++ app/src/main/java/com/bloch1790/exchangerate/util/HttpCallbackListener.java
@@ -0,0 +1,10 @@
+package com.bloch1790.exchangerate.util;
+
+/**
+ * Created by Administrator on 2016/2/25 0025.
+ */
+public interface HttpCallbackListener {
+    void onFinish(String response);
+
+    void onError(Exception e);
+}
